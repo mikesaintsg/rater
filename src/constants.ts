29ed86e994b3d8b0c9d02f06942ab69f69e0bc65
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2026-01-18 15:17:39 -0500

    Add constants for rater library configuration

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * @mikesaintsg/rater
+ *
+ * Constants for the rater library.
+ */
+
+/** Default base rate */
+export const DEFAULT_BASE_RATE = 0
+
+/** Default decimal places for rounding */
+export const DEFAULT_DECIMAL_PLACES = 2
+
+/** Default continue on error setting */
+export const DEFAULT_CONTINUE_ON_ERROR = true
+
+/** Default group aggregation method */
+export const DEFAULT_GROUP_AGGREGATION_METHOD = 'sum' as const
